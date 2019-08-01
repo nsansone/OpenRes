c680c8f9b76da9470da2ccc6b45d9e3de8809f2f
Nicole Sansone <nsansone93@gmail.com>
2019-07-31 20:09:14 -0400

    import close modal for signup form container

--- frontend/components/session_form/signup_form_container.jsx
+++ frontend/components/session_form/signup_form_container.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { signup } from '../../actions/session_actions';
 import SignupForm from './session_form';
+import { openModal, closeModal } from '../../actions/modal_actions';
 
 const mapStateToProps = ({ errors }) => {
     return {
