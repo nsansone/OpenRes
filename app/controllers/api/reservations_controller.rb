c7eddcc92b2fe93d97a9a6be96d314f92fd89bde
K. Gofron <kgofron@comcast.net>
2019-08-05 21:10:46 -0400

    start reservations backend

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -1,5 +1,17 @@
 class ReservationsController < ApplicationController 
 
+    def create
+        @reservation = current_user.reservations.new(reservation_params)
+        if @reservation.save
+            render :show
+        else
+            render json: @reservation, status: 422
+        end
+    end
+
+    def reservation_params
+        params.require(:reservation).permit(:date, :time, :party_size)
+    end
 
     
 
