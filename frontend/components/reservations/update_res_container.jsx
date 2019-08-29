fb1d9537322222879ed50701a0deeda3a8899fbf
Nicole Sansone <nsansone93@gmail.com>
2019-08-29 13:44:26 -0400

    fix syntax error

--- frontend/components/reservations/update_res_container.jsx
+++ frontend/components/reservations/update_res_container.jsx
@@ -5,8 +5,8 @@ import { connect } from 'react-redux';
 const mapStateToProps = (state, ownProps) => {
   
   return ({
-    restaurant: state.entities.restaurants[ownProps.match.params.restaurantId] || {}
-    reservation: state.entities.reservations[parseInt(ownProps.match.params.reservationId)] || {  },
+    restaurant: state.entities.restaurants[ownProps.match.params.restaurantId] || {},
+    reservation: state.entities.reservations[parseInt(ownProps.match.params.reservationId)] || {},
     currentUser: state.entities.users[state.session.id]
   });
 };
